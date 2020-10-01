a583262eaa78a5ede067bab75cca7e44c43b9860
Mohamad AL Hassan <70607967+mhmdlhsn97@users.noreply.github.com>
2020-10-01 15:12:33 +0300

    File/Dependency creation.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is project will use HTML/CSS/JS/PHP/MySQL to create a Todo List Web App.
+Css: Bootstrap.
+PHP ran by Apache server that's included in xampp(v3.2.4)
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up/In</title>
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <script src="JS/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="FA/css/all.min.css">
+    <link rel="stylesheet" href="CSS/custom.css">
+    <script src="FA/js/all.min.js"></script>
+</head>
+<body>
+    <?php 
+    include 'pages/signin.html';
+    ?>
+</body>
+</html>
