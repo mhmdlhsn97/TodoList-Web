f1c9202a7247d462e12e312eeb64172cb290e51f
Mohamad AL Hassan <70607967+mhmdlhsn97@users.noreply.github.com>
2020-10-02 13:59:15 +0300

    Added DB conn and Initial JS to test Sign UP

--- JS/custom.js
+++ JS/custom.js
@@ -11,3 +11,20 @@ function gotoSignIn() {
         this;
     });
 };
+
+//Sign Up Ajax cmnd
+function signUp(){
+    var username=$('#user-name').val();
+    var email=$('#user-mail').val();
+    var pass=$('#pswd').val();
+    var passCnf=$('#pswd-cnf').val();
+    if(pass == passCnf){
+        console.log("Log SignUp\n///////////////////////\n"+
+        "The username : "+username+"\nThe Email: "+email+"\nThe Pass: "+pass+"\n///////////////////////\n");
+    }else{
+        $('#passCnf').empty();
+        $('#passCnf').append("Password didn't match");
+        $('#passCnf').attr('style', 'display:block,');
+
+    }
+}
---
+++ db/conn.php
@@ -0,0 +1,23 @@
+<?php
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=todo-list", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  console_log( "Connected successfully");
+} catch(PDOException $e) {
+  console_log( "Connection failed: " . $e->getMessage());
+}
+
+?>
